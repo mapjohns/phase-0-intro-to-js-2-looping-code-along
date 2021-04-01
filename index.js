32d3044b8329629591c17e510b51484ae698cc8a
Matthew <mattj20@gmail.com>
2021-04-01 15:00:43 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+let giftMessage = []
+
+function writeCards(array, message) {
+    for (let i =0; i<array.length; i++) {
+        console.log(`Thank you, ${array[i]}, for the wonderful ${message} gift!`)
+        giftMessage[i] = `Thank you, ${array[i]}, for the wonderful ${message} gift!`
+
+    }
+    return giftMessage
+}
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+countDown(10)
